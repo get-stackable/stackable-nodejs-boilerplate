c5976ba78555fc0a5538db4fb1d7c16220f4b3f1
Parminder Klair <perminder.klair@gmail.com>
2016-03-09 21:29:42 +0000

    updated stackable

--- controllers/main.js
+++ controllers/main.js
@@ -6,10 +6,10 @@ exports.index = function(req, res) {
     var stackable = req.res.stackable;
 
     stackable.getContainerItems('qnwesbEkW7kcjqzZ7', function (error, result) {
-        console.log(error, result);
-    });
+        //console.log(error, result);
 
-    res.render('home', {
-        title: 'Home'
+        res.render('home', {
+            projects: result.data
+        });
     });
 };
